4e384300eb187a61aa55985a9ae053837e1c013b
Gabriel Carramate <glcarramate@hotmail.com>
2022-11-17 20:35:41 -0300

    delete react logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './assets/icons/logo.svg';
 import { H1, H2 } from './atomic/atm.typography/typography.component.style';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <H1>
           Edit <H2>src/App.tsx</H2> and save to reload.
         </H1>
